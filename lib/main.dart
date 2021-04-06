2e3bca6b094c91f247581f7df7941a6c0f9c439c
Nick Wylynko <nick1014375@gmail.com>
2021-04-06 14:40:44 +0800

    add meals menu

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late Future<Categories> futureCategories;
-  String selectedCategory = '';
+  late Future<Meals> futureMeals;
+  bool showMealsPage = false;
 
   @override
   void initState() {
@@ -37,17 +38,20 @@ class _MyAppState extends State<MyApp> {
                 futureCategories: futureCategories,
                 onTapCategory: _handleCategoryTapped),
           ),
-          if (selectedCategory != '')
-            MaterialPage(child: Text('${selectedCategory}')),
+          if (showMealsPage)
+            MaterialPage(
+                child: MealsPage(
+              futureMeals: futureMeals,
+              onTapMeal: _handleMealsTapped,
+            )),
         ],
         onPopPage: (route, result) {
           if (!route.didPop(result)) {
             return false;
           }
 
-          // Update the list of pages by setting _selectedBook to null
           setState(() {
-            selectedCategory = '';
+            showMealsPage = false;
           });
 
           return true;
@@ -56,14 +60,171 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  void _handleCategoryTapped(String categoryId) {
+  void _handleCategoryTapped(String categoryName) {
     setState(() {
-      selectedCategory = categoryId;
+      futureMeals = fetchMeals(categoryName);
+      showMealsPage = true;
+    });
+  }
+
+  void _handleMealsTapped(String categoryName) {
+    setState(() {
+      // futureMeals = fetchMeals(categoryName);
+      // showMealsPage = true;
     });
   }
 }
 
-// HomePage(futureCategories: futureCategories)
+class MealsPage extends StatelessWidget {
+  const MealsPage(
+      {Key? key, required this.futureMeals, required this.onTapMeal})
+      : super(key: key);
+
+  final Future<Meals> futureMeals;
+  final ValueChanged<String> onTapMeal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meal Database App'),
+      ),
+      body: Center(
+        child: MealsScreen(futureMeals: futureMeals, onTapMeal: onTapMeal),
+      ),
+    );
+  }
+}
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen(
+      {Key? key, required this.futureMeals, required this.onTapMeal})
+      : super(key: key);
+
+  final Future<Meals> futureMeals;
+  final ValueChanged<String> onTapMeal;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Meals>(
+      future: futureMeals,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // return Text(snapshot.data!.Meals.first.strMeal);
+          return MealsList(
+            meals: snapshot.data!.meals,
+            onTapMeal: onTapMeal,
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+
+        // By default, show a loading spinner.
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
+
+class MealsList extends StatelessWidget {
+  const MealsList({
+    required this.meals,
+    required this.onTapMeal,
+    Key? key,
+  }) : super(key: key);
+
+  final List<Meal> meals;
+  final ValueChanged<String> onTapMeal;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: meals.length,
+        itemBuilder: (BuildContext context, int index) {
+          return MealsItem(meal: meals[index], onTap: onTapMeal);
+        });
+  }
+}
+
+class MealsItem extends StatelessWidget {
+  const MealsItem({
+    required this.meal,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  final Meal meal;
+  final ValueChanged<String> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Text(meal.strMeal),
+        leading: Image.network(meal.strMealThumb),
+        minVerticalPadding: 32,
+        onTap: () => onTap(meal.strMeal));
+  }
+}
+
+Future<Meals> fetchMeals(String categoryName) async {
+  final response = await http.get(Uri.https(
+      "www.themealdb.com", 'api/json/v1/1/filter.php', {"c": categoryName}));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return mealsFromJson(response.body);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Meals');
+  }
+}
+
+Meals mealsFromJson(String str) => Meals.fromJson(json.decode(str));
+
+String mealsToJson(Meals data) => json.encode(data.toJson());
+
+class Meals {
+  Meals({
+    required this.meals,
+  });
+
+  List<Meal> meals;
+
+  factory Meals.fromJson(Map<String, dynamic> json) => Meals(
+        meals: List<Meal>.from(json["meals"].map((x) => Meal.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "meals": List<dynamic>.from(meals.map((x) => x.toJson())),
+      };
+}
+
+class Meal {
+  Meal({
+    required this.strMeal,
+    required this.strMealThumb,
+    required this.idMeal,
+  });
+
+  String strMeal;
+  String strMealThumb;
+  String idMeal;
+
+  factory Meal.fromJson(Map<String, dynamic> json) => Meal(
+        strMeal: json["strMeal"],
+        strMealThumb: json["strMealThumb"],
+        idMeal: json["idMeal"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "strMeal": strMeal,
+        "strMealThumb": strMealThumb,
+        "idMeal": idMeal,
+      };
+}
 
 class HomePage extends StatelessWidget {
   const HomePage(
@@ -223,6 +384,6 @@ class CategoryItem extends StatelessWidget {
         title: Text(category.strCategory),
         leading: Image.network(category.strCategoryThumb),
         minVerticalPadding: 32,
-        onTap: () => onTap(category.idCategory));
+        onTap: () => onTap(category.strCategory));
   }
 }
